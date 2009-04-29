4f69a07681e34183bc4619dd5644328fdbb96989
Brent Snook <brent@fuglylogic.com>
2009-04-29 15:07:27 +0200

    implemented scenario for straight hundred millions (e.g. seven hundred million)

--- features/recognise_millions.feature
+++ features/recognise_millions.feature
@@ -12,6 +12,10 @@ Feature: Recognise millions
     When 'six hundred and thirty seven million' is parsed
     Then the number will be 637,000,000
 
+  Scenario: Recognise several hundred million straight
+    When 'seven hundred million' is parsed
+    Then the number will be 700,000,000
+
   Scenario: Recognise numbers between millions
     When 'six hundred and thirty seven million, four hundred and thirty eight thousand, two hundred and ninety eight' is parsed
     Then the number will be 637,438,298
--- lib/numerouno.rb
+++ lib/numerouno.rb
@@ -86,11 +86,7 @@ module Numerouno
     end  
   end
 
-  class Amalgamation
-    
-    def initialize power
-      @power = power
-    end  
+  class Amalgamation 
     
     def self.apply_for power
       new(power)
@@ -101,20 +97,26 @@ module Numerouno
       @numbers.each_with_index do |number, index|
         @current = index
         if number_is_correct_power?
-          apply :*, to_left if number_to_left? and @power > 10
-          apply :+, to_right if number_to_right?
+          apply :*, to_left if should_multiply?
+          apply :+, to_right if should_add?
         end 
       end
       
       @numbers.compact!
     end
     
-    def number_to_left?
-      @current > 0
+    private
+
+    def initialize power
+      @power = power
+    end
+       
+    def should_multiply?
+      @current > 0 and @power > 10
     end 
     
-    def number_to_right?
-      @current < (@numbers.length - 1)
+    def should_add?
+      @current < (@numbers.length - 1) and number > @numbers[to_right]
     end 
     
     def apply operation, index
