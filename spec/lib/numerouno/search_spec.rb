0eb2261c64d2e7e40f30883dac22e3cb4569af42
Brent Snook <brent@fuglylogic.com>
2009-05-03 11:31:15 +0200

    added spec for Search and SearchMethods

---
+++ spec/lib/numerouno/search_spec.rb
@@ -0,0 +1,32 @@
+require File.dirname(__FILE__) + '/../../spec_helper.rb'
+
+describe Numerouno::Search do
+  
+  # a lot of the search functionality is already covered in the features
+  
+  it 'should locate all numbers within a string' do
+    search_for('seventy five thousand and forty two').should eql([70, 5, 1000, 40, 2])
+  end  
+  
+  it 'should not falsely identify partial numbers in powers of ten' do
+    search_for('sixty seventy eighty ninety').should eql([60, 70, 80, 90])
+  end
+  
+  it 'should not falsely identify partial numbers in teens' do
+    search_for('fourteen sixteen seventeen eighteen nineteen').should eql([14, 16, 17, 18, 19])
+  end
+  
+  def search_for string
+    Numerouno::Search.new(string).find_all
+  end 
+end
+
+describe Numerouno::SearchMethods do
+  
+  include Numerouno::SearchMethods
+  
+  it 'should allow a new search to be easily created' do
+    numbers_within('one two three').should eql([1, 2, 3])  
+  end
+  
+end
