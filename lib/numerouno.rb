6f577996bbe9d82d4ff257b009a86d1115d5412e
Brent Snook <brent@fuglylogic.com>
2009-05-03 12:36:28 +0200

    made a start on README and added module parse method

--- lib/numerouno.rb
+++ lib/numerouno.rb
@@ -1,21 +1,11 @@
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-
-[
-  'combiner',
-  'search',
-  'parser'
-].each {|file| require "numerouno/#{file}"}
+require File.join(File.dirname(__FILE__), 'numerouno-parsing')
 
 module Numerouno
-  VERSION = '0.0.1'
-        
+
   module StringExtensions
-    
-    include Parser
-    
+  
     def parse_i
-      Parser.number_from self
+      Numerouno.parse self
     end
   end
      
